088df8f52e525a34bc329420317b0d4be7767480
Arnór Valdimarsson <arnorv@gmail.com>
2016-03-20 15:11:36 +0000

    dbHelper

---
+++ development/src/dbHelper.java
@@ -0,0 +1,22 @@
+/**
+ * Created by arnorv on 20/03/16.
+ */
+public class dbHelper {
+    private String getBookingStr;
+    private String saveBookingStr;
+    private String getHotelStr;
+    private String saveHotelStr;
+    private String getReviewStr;
+    private String saveReviewStr;
+    private String getRoomStr;
+    private String saveRoomStr;
+    private String[] params;
+
+    public void runQuery(String queryStr, String[] params) {
+        return;
+    }
+
+    public void setParams(String[] newParams) {
+        params = newParams;
+    }
+}

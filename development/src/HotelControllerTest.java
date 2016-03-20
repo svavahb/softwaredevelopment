cf18db433e794da22822d5cc82fb8a4c9b2c3cd0
Svava <svavahb@gmail.com>
2016-03-20 16:12:45 +0000

    testingtesting

---
+++ development/src/HotelControllerTest.java
@@ -0,0 +1,52 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Svava Hildur on 20.3.2016.
+ */
+public class HotelControllerTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSaveHotel() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testDeleteHotel() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGiveReview() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testAddRoom() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testRemoveRoom() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testUpdateRoom() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testFindHotelWithAvailableRooms() throws Exception {
+
+    }
+}

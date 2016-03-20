831f5595d4cfa3117c04d143b21e0001cdb329fd
Svava <svavahb@gmail.com>
2016-03-20 16:05:32 +0000

    testingtesting

---
+++ development/src/BookingControllerTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Svava Hildur on 20.3.2016.
+ */
+public class BookingControllerTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testBookingController() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testSaveBooking() throws Exception {
+
+    }
+}

f756ca65493970dc74a3dd1b3dfaf600d36cf3d5
Svava <svavahb@gmail.com>
2016-03-23 13:38:15 +0000

    testingtesting

--- development/src/BookingControllerTest.java
+++ development/src/BookingControllerTest.java
@@ -7,21 +7,36 @@ public class BookingControllerTest {
 
     @org.junit.Before
     public void setUp() throws Exception {
-
+        BookingController bcontroller = new BookingController();
+        Booking book = new Booking(1);
+        book.setCustomerName("palli");
     }
 
     @org.junit.After
     public void tearDown() throws Exception {
-
+        bcontroller = null;
     }
 
     @org.junit.Test
     public void testBookingController() throws Exception {
-
+        assertNotNull(bcontroller);
     }
 
     @org.junit.Test
-    public void testSaveBooking() throws Exception {
+    public void testgetBooking() throws Exception {
+        Booking btest = bcontroller.getBooking(1);
+        assertNotNull(btest);
+    }
 
+    @org.junit.Test
+    public void testgetBookings() throws Exception {
+        Booking[] bfylki = bcontroller.getBookings();
+        assertNotNull(bfylki);
+    }
+
+    @org.junit.Test
+    public void testgetBookingsByCustomer() throws Exception {
+        Booking[] pallibook = getBookingsByCustomer("palli");
+        assertNotNull(pallibook);
     }
 }

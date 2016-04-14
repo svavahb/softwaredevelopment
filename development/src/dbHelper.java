d4036e359eb2379bab70a757bf3b60bb12042840
Svava <svavahb@gmail.com>
2016-04-14 19:21:20 +0000

    database fikt

--- development/src/dbHelper.java
+++ development/src/dbHelper.java
@@ -64,6 +64,7 @@ public class dbHelper {
             }
             resultList.add(result.getArray(10));
             bla.add(row);
+            //nfdjb
         }
         String tmp = resultList.get(0).toString();
         tmp = tmp.substring(1, tmp.length() - 1);
